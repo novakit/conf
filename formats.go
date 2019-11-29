da5b902a94d224baabcd8d3925b3bbbcfba1bfbd
Guo Y.K <hi@guoyk.net>
2019-11-29 20:53:27 +0800

    add support for toml

--- formats.go
+++ formats.go
@@ -2,12 +2,13 @@ package conf
 
 import (
 	"encoding/json"
+	"github.com/BurntSushi/toml"
 	"gopkg.in/yaml.v2"
 )
 
 type Format struct {
 	Exts   []string
-	ToJSON func(content []byte) ([]byte, error)
+	ToJSON func(content []byte) (out []byte, err error)
 }
 
 var (
@@ -23,6 +24,16 @@ var (
 			ToJSON: func(content []byte) (bytes []byte, err error) {
 				var m map[string]interface{}
 				if err = yaml.Unmarshal(content, &m); err != nil {
+					return
+				}
+				return json.Marshal(m)
+			},
+		},
+		"TOML": {
+			Exts: []string{"toml"},
+			ToJSON: func(content []byte) (out []byte, err error) {
+				var m map[string]interface{}
+				if err = toml.Unmarshal(content, &m); err != nil {
 					return
 				}
 				return json.Marshal(m)
--- formats_test.go
+++ formats_test.go
@@ -19,4 +19,9 @@ func TestFormats(t *testing.T) {
 	out, err = Formats["YAML"].ToJSON(buf)
 	require.NoError(t, err)
 	require.Equal(t, []byte(`{"hello":"world"}`), out)
+
+	buf = []byte(`hello = "world"`)
+	out, err = Formats["TOML"].ToJSON(buf)
+	require.NoError(t, err)
+	require.Equal(t, []byte(`{"hello":"world"}`), out)
 }
